d2db68cfe9f31102376b75bbb9ed985a50565585
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-06-09 06:34:58 +0000

    Added Fibonacci sequence endpoint and cleaned up code

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -6,81 +6,64 @@ const htmlBoilerPlate =
   "<!DOCTYPE html><html><head><title>TS-NODE-EXPRESS</title></head><body>{0}</body></html>";
 
 // Get system information
-export const getSysInfo = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    let [cpu, mem, graphics, os] = await Promise.all([
-      si.cpu(),
-      si.mem(),
-      si.graphics(),
-      si.osInfo(),
-    ]);
+export const getSysInfo = async (req: Request, res: Response) => {
+  let [cpu, mem, graphics, os] = await Promise.all([
+    si.cpu(),
+    si.mem(),
+    si.graphics(),
+    si.osInfo(),
+  ]);
 
-    let defaultNetworkInterface = await si.networkInterfaces("default");
-    let ip_address = defaultNetworkInterface.ip4;
-    let network_type = defaultNetworkInterface.type;
-    let network_speed = defaultNetworkInterface.speed;
+  let defaultNetworkInterface = await si.networkInterfaces("default");
+  let ip_address = defaultNetworkInterface.ip4;
+  let network_type = defaultNetworkInterface.type;
+  let network_speed = defaultNetworkInterface.speed;
 
-    let graphicsInfo =
-      graphics.controllers.length > 0
-        ? `${graphics.controllers[0].model} (VRAM: ${graphics.controllers[0].vram})`
-        : "";
+  let graphicsInfo =
+    graphics.controllers.length > 0
+      ? `${graphics.controllers[0].model} (VRAM: ${graphics.controllers[0].vram})`
+      : "";
 
-    var content = "";
-    let items = [
-      { key: "Host Name", value: os.hostname },
-      {
-        key: "IP Address",
-        value: `${ip_address} (${network_type}, ${network_speed} Mbit / s)`,
-      },
-      {
-        key: "CPU",
-        value: `${cpu.manufacturer} ${cpu.brand} ${cpu.speed} GHz (${process.arch})`,
-      },
-      { key: "Memory", value: `${mem.total / 1000000000} GB` },
-      { key: "Graphic", value: graphicsInfo },
-      {
-        key: "OS",
-        value: `${os.distro} ${os.release} ${os.codename} ${os.kernel}`,
-      },
-    ];
-    content += "<table cellpadding='10'>";
-    for (var item of items) {
-      content +=
-        "<tr><td>" + item["key"] + "</td><td>" + item["value"] + "</td></tr>";
-    }
-    content += "</table>";
-
-    res.status(200).send(htmlBoilerPlate.replace("{0}", content));
-  } catch (error) {
-    next(error);
+  var content = "";
+  let items = [
+    { key: "Host Name", value: os.hostname },
+    {
+      key: "IP Address",
+      value: `${ip_address} (${network_type}, ${network_speed} Mbit / s)`,
+    },
+    {
+      key: "CPU",
+      value: `${cpu.manufacturer} ${cpu.brand} ${cpu.speed} GHz (${process.arch})`,
+    },
+    { key: "Memory", value: `${mem.total / 1000000000} GB` },
+    { key: "Graphic", value: graphicsInfo },
+    {
+      key: "OS",
+      value: `${os.distro} ${os.release} ${os.codename} ${os.kernel}`,
+    },
+  ];
+  content += "<table cellpadding='10'>";
+  for (var item of items) {
+    content +=
+      "<tr><td>" + item["key"] + "</td><td>" + item["value"] + "</td></tr>";
   }
+  content += "</table>";
+
+  res.status(200).send(htmlBoilerPlate.replace("{0}", content));
 };
 
 // Get environment variables
-export const getEnvVar = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    var envVar =
-      "<table cellpadding='10'><tr><th align='left'>Key</th><th align='left'>Value</th></tr>";
+export const getEnvVar = async (req: Request, res: Response) => {
+  var envVar =
+    "<table cellpadding='10'><tr><th align='left'>Key</th><th align='left'>Value</th></tr>";
 
-    const sortedKeys = Object.keys(process.env).sort();
-    for (const key of sortedKeys) {
-      envVar +=
-        "<tr><td>" + key + "</td><td>" + process.env[key] + "</td></tr>";
-    }
-    envVar += "</table>";
-
-    res.send(htmlBoilerPlate.replace("{0}", envVar));
-  } catch (error) {
-    next(error);
+  const sortedKeys = Object.keys(process.env).sort();
+  for (const key of sortedKeys) {
+    envVar += "<tr><td>" + key + "</td><td>" + process.env[key] + "</td></tr>";
   }
+  envVar += "</table>";
+
+  res.send(htmlBoilerPlate.replace("{0}", envVar));
 };
 
 // Redirects to another host and path
@@ -104,4 +87,27 @@ export const redirect = async (
     .catch((error) => {
       next(error);
     });
+};
+
+// Fibonacci sequence
+export const fibonacci = async (req: Request, res: Response) => {
+  const n = parseInt(req.query.n as string) || 1;
+  if (isNaN(n)) {
+    res.status(400).send("Invalid number");
+  } else {
+    const start = Date.now();
+    const fib_result = await fib(n);
+    const end = Date.now();
+    const result = {
+      architecture: process.arch,
+      fibonacci: { n: n, result: fib_result },
+      timeTaken: end - start + " ms",
+    };
+    res.status(200).send(result);
+  }
+};
+
+const fib = async (n: number): Promise<number> => {
+  if (n <= 1) return n;
+  return (await fib(n - 1)) + (await fib(n - 2));
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getSysInfo, getEnvVar, redirect } from "../controllers/controller";
+import {
+  getSysInfo,
+  getEnvVar,
+  redirect,
+  fibonacci,
+} from "../controllers/controller";
 
 const router = Router();
 
 router.get("/", getSysInfo);
 router.get("/env-var", getEnvVar);
+router.get("/fib", fibonacci);
 router.get("/redirect", redirect);
 
 export default router;
