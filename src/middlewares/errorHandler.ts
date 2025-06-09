0481c27affbcbe69a34d8fc860a22b0d1c88e6fc
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-06-09 06:34:22 +0000

    Fixed issues with error handling middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,13 +1,18 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { log } from "../log";
 
 export interface AppError extends Error {
   status?: number;
 }
 
-export const errorHandler = (err: AppError, req: Request, res: Response) => {
-  log.error(err);
+export const errorHandler = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  log.error(err.message);
   res.status(err.status || 500).json({
-    message: err.message || "Internal Server Error",
+    error: err.message || "Internal Server Error",
   });
 };
