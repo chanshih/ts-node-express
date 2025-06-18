375a49a4bb545e7eeb667f603f39ccdb1ea64518
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-06-18 06:39:50 +0000

    Refactored code

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ app.use(express.json());
 
 // Routes
 app.use("/", routes);
+const path_prefixes = process.env.PATH_PREFIXES?.split(",") || [];
+for (const prefix of path_prefixes) {
+  app.use(prefix, routes);
+}
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,20 +8,9 @@ import {
 
 const router = Router();
 
-const paths = [
-  { path: "/env-var", handler: getEnvVar },
-  { path: "/fib", handler: fibonacci },
-  { path: "/req", handler: proxyRequest },
-  { path: "/", handler: getSysInfo },
-];
-
-const path_prefixes = process.env.PATH_PREFIXES?.split(",") || [];
-
-for (const path of paths) {
-  router.get(path.path, path.handler);
-  for (const prefix of path_prefixes) {
-    router.get(prefix + path.path, path.handler);
-  }
-}
+router.get("/env-var", getEnvVar);
+router.get("/fib", fibonacci);
+router.get("/req", proxyRequest);
+router.get("/", getSysInfo);
 
 export default router;
